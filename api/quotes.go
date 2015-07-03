77c552f5ca5d4d1d1874a4a219c23c3e4ad8aec8
Wijnand van Deutekom <wijnand@zerok.nl>
2015-07-03 12:35:18 +0200

    Cleaned up code, preparing for rethink integration

--- api/quotes.go
+++ api/quotes.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -20,16 +19,17 @@ type QuoteJSON struct {
 }
 
 type Quote struct {
-	Token       string `schema:"token"`
-	TeamID      string `schema:"team_id"`
-	TeamDomain  string `schema:"team_domain"`
-	ChannelID   string `schema:"channel_id"`
-	ChannelName string `schema:"channel_name"`
-	Timestamp   int    `schema:"timestamp"`
-	UserID      string `schema:"user_id"`
-	UserName    string `schema:"user_name"`
-	Text        string `schema:"text"`
-	TriggerWord string `schema:"trigger_word"`
+	Token       string  `schema:"token"`
+	TeamID      string  `schema:"team_id"`
+	TeamDomain  string  `schema:"team_domain"`
+	ChannelID   string  `schema:"channel_id"`
+	ChannelName string  `schema:"channel_name"`
+	Timestamp   float32 `schema:"timestamp"`
+	UserID      string  `schema:"user_id"`
+	UserName    string  `schema:"user_name"`
+	Text        string  `schema:"text"`
+	Command     string  `schema:"command"`
+	ServiceID   string  `schema:"service_id"`
 }
 
 // type Quote struct {
@@ -51,68 +51,18 @@ type Response struct {
 }
 
 func (a *AppContext) newQuote(c *echo.Context) error {
+	//Debug
+	//body, _ := ioutil.ReadAll(c.Request().Body)
 
 	r := c.Request()
 
 	r.ParseMultipartForm(5120)
 	isValid := len(r.Form["text"]) > 0 && len(r.Form["team_id"]) > 0
 	if !isValid {
-		log.Println("see ya homies")
+		log.Println("Invalid form (empty?)\nI'm a doctor Jim, not a magician!")
 	}
-	//	isCmd := len(r.Form["trigger_word"]) > 0
 
-	fmt.Printf("TRIGGERRR: %s", r.Form["trigger_word"][0])
-
-	body, _ := ioutil.ReadAll(c.Request().Body)
-	contentType := c.Request().Header.Get("Content-Type")
-
-	log.Printf("form parsed1: ", r.Form)
-	r.ParseForm()
-	log.Printf("form parsed2: ", r.Form)
-	log.Printf("form value§: ", r.FormValue("token"))
-	log.Printf("form value§: ", r.PostFormValue("token"))
-
-	x := r.Form.Get("token")
-	fmt.Printf("param_name: ", x)
-
-	fmt.Printf("request method: %s\n", c.Request().Method)
-	fmt.Printf("formvalue: %s\n", c.Request().FormValue("token"))
-	fmt.Printf("content-type: %s\n", contentType)
-	fmt.Printf("response Body: %s\n", string(body))
-	fmt.Printf("Post form get token: %s\n", c.Request().PostForm.Get("token"))
-	fmt.Printf("Post form get token222: %s\n", c.Request().PostFormValue("token"))
-
-	// var u Quote
-
-	// defer c.Request().Body.Close()
-
-	// if err := json.NewDecoder(c.Request().Body).Decode(&u); err != nil {
-	// 	fmt.Printf("json decoded inside if: %#v\n", u)
-	// }
-
-	// fmt.Printf("json decoded: %#v\n", u)
-
-	// d := form.NewDecoder(c.Request().Body)
-	// if err := d.Decode(&u); err != nil {
-	// 	fmt.Printf("ERROR JIM! ERROR!: ", err)
-	// }
-
-	// fmt.Printf("Decoded: %#v", u)
-
-	// payload := make(map[string]interface{})
-
-	// if strings.Contains(contentType, "form") {
-	// 	fmt.Println("Looks like youre sending a form")
-
-	// 	formData, err := url.ParseQuery(string(body))
-	// 	if err != nil {
-	// 		log.Printf("error parsing form payload %+v\n", err)
-	// 	} else {
-	// 		payload = valuesToMap(formData)
-	// 	}
-	// }
-
-	// fmt.Printf("payload payload paylod: %#v\n", payload)
+	fmt.Printf("form:: %s\n", r.Form)
 
 	quote := new(Quote)
 	decoder := schema.NewDecoder()
@@ -123,49 +73,11 @@ func (a *AppContext) newQuote(c *echo.Context) error {
 		//log.Printf("error %s", string.err.Error)
 		// Handle error
 	}
-
-	fmt.Printf("THIS ISNT EVEN MY FINAL FORM: %#v\n", quote)
-	//c.Request().ParseForm()
-	//newQ := Quote{
-	//	Token:  c.Request().Form.Get("token"),
-	//	TeamID: c.Request().Form.Get("team_id"),
-	//	Token:  c.Request().Form.Get("team_domain"),
-	//	Token:  c.Request().Form.Get("channel_id"),
-	//	Token:  c.Request().Form.Get("channel_name"),
-	//	Token:  c.Request().Form.Get("timestamp"),
-	//	Token:  c.Request().Form.Get("user_id"),
-	//	Token:  c.Request().Form.Get("user_name"),
-	//	Token:  c.Request().Form.Get("text"),
-	//	Token:  c.Request().Form.Get("trigger_word"),
-	//}
-
-	//json.Unmarshal([]byte(body), &quote)
-	//log.Printf("%#v\n", quote)
-
-	//uID := "U02QG1PBN"  //wijnand
-	//chID := "C02QG1PDQ" //Generaal
-
-	//quote := Quote{
-	//	Token:       "lXofu0WHVRJpB0P1efGSNVyl",
-	//	TeamID:      "teamid931",
-	//	TeamDomain:  "itlounge",
-	//	ChannelID:   chID,
-	//	ChannelName: "generaal",
-	//	Timestamp:   1435839624,
-	//	UserID:      uID,
-	//	UserName:    "wijnand",
-	//	Text:        "JA HALLO!",
-	//	TriggerWord: "quote",
-	//}
-	//resp := QuoteJSON{
-	//	Data: quote,
-	//}
-
-	//bytes, _ := json.Marshal(quote)
+	fmt.Printf("Filled quote: %#v\n", quote)
 
 	resp := Response{
-		Username: "Gopher :)",
-		Text:     "JA HALLO!",
+		Username: quote.UserName,
+		Text:     "Saving quote: " + quote.Text,
 	}
 
 	fmt.Println("\n\n\n")
